85d92613a659425c61eb6d7ed394da76e9b7e929
vfkon <vitfvk@gmail.com>
2020-10-15 20:55:31 +0400

    прогресс епта

--- nikpo 1/Source.cpp
+++ nikpo 1/Source.cpp
@@ -6,7 +6,11 @@ private:
 	Node*parent;
 	std::vector<Node*> children;
 	int value;
+	bool IsDegenerate;
 public:
+	Node* GetParent() {
+		return parent;
+	}
 	Node* GetChild(int index) {
 		return children[index];
 	}
@@ -47,25 +51,42 @@ public:
 //		generatebranches(root->GetChild(i),nodesleft-amount,level+1);
 //	}
 //}
-int generatebranches1(Node* root, int nodes,int level, int neededlevel) {
-	if (root->GetAmountofChildren() != 0) {
-		for (int i = 0; i < root->GetAmountofChildren(); i++) {
-			nodes=generatebranches1(root->GetChild(i),nodes,level+1, neededlevel);
+int generatebranches1(Node* root, int nodes,int level, int neededlevel,int totalnodes) {
+	int oldnodes = nodes;
+	int amount = 0;
+	if (level == neededlevel) {//мы на правильном уровне
+		srand(time(0));
+		amount = rand() % 6;
+		for (int i = 0; i < amount; i++) {
+			root->AddNode(root, 10 * level + i);//сгенерировать ветви
 		}
+		//if ((oldnodes = nodes) && (!root->GetParent())) {
+		//	return -1;
+		//}
+		//if (level != neededlevel) {
+		//	if (root->GetParent())return nodes;//возврат если для этого листа в какой-то момент рандом выдал 0, лист тупиковый
+		//	else return
+		//}
+		
 	}
-	if (level != neededlevel)return nodes;//возврат если для этого листа в какой-то момент рандом выдал 0, лист тупиковый
-	srand(time(0));
-	int amount = rand() % 6;
-	for (int i = 0; i < amount; i++) {
-		root->AddNode(root, 10 * level + i);
+	else { //мы на неправильном уровне
+		if (root->GetAmountofChildren() != 0) {//но дальше есть еще ветви
+			for (int i = 0; i < root->GetAmountofChildren(); i++) {
+				nodes = generatebranches1(root->GetChild(i), nodes, level + 1, neededlevel); //идти дальше по дереву
+			}
+		}
+		else {//дальше ветвей нет
+			if(totalnodes-nodes<10)
+		}
 	}
 	return nodes - amount;
 }
 void gentree(int nodes) {
 	Node* root = new Node(0);
 	int neededlevel = 0;
+	int totalnodes = nodes;
 	while (nodes > 0) {
-		nodes = generatebranches1(root, nodes, 0, neededlevel);
+		nodes = generatebranches1(root, nodes, 0, neededlevel,totalnodes);
 		neededlevel++;
 	}
 	delete root;
