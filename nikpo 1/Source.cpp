f24af234241c52884006c8d4ef83e9390273a0c4
vfkon <vitfvk@gmail.com>
2020-10-20 20:45:07 +0400

    Добавлен поиск листьев, исправлено удаление дерева

--- nikpo 1/Source.cpp
+++ nikpo 1/Source.cpp
@@ -6,7 +6,7 @@ private:
 	Node*parent;
 	std::vector<Node*> children;
 	int value;
-	bool IsDegenerate;
+	//bool IsDegenerate;
 public:
 	Node* GetParent() {
 		return parent;
@@ -17,13 +17,16 @@ public:
 	int GetAmountofChildren() {
 		return children.size();
 	}
+private:
 	void DestroyTree(Node* current) {
-		for (int i = 0; i < children.size(); i++) {
-			DestroyTree(children[i]);
-			delete children[i];
+		std::vector<Node*> ptr;
+		for (int i = 0; i < GetAmountofChildren(); i++) {
+			delete current->GetChild(i);
+			//current->GetChild(i);
 		}
-		children.clear();
+		current->children.erase(children.begin(), children.end());
 	}
+public:
 	void AddNode(Node*root,int initval) {
 		Node *leaf = new Node(initval,root);
 		children.push_back(leaf);
@@ -51,14 +54,15 @@ public:
 //		generatebranches(root->GetChild(i),nodesleft-amount,level+1);
 //	}
 //}
-int generatebranches1(Node* root, int nodes,int level, int neededlevel,int totalnodes) {
+int generatebranches1(Node* root, int nodes, int level, int neededlevel, int totalnodes) {
 	int oldnodes = nodes;
 	int amount = 0;
 	if (level == neededlevel) {//мы на правильном уровне
-		srand(time(0));
+		//srand(time(0));
 		amount = rand() % 6;
 		for (int i = 0; i < amount; i++) {
-			root->AddNode(root, 10 * level + i);//сгенерировать ветви
+			nodes--;
+			root->AddNode(root, totalnodes-nodes);//сгенерировать ветви
 		}
 		//if ((oldnodes = nodes) && (!root->GetParent())) {
 		//	return -1;
@@ -67,29 +71,49 @@ int generatebranches1(Node* root, int nodes,int level, int neededlevel,int total
 		//	if (root->GetParent())return nodes;//возврат если для этого листа в какой-то момент рандом выдал 0, лист тупиковый
 		//	else return
 		//}
-		
+
 	}
 	else { //мы на неправильном уровне
 		if (root->GetAmountofChildren() != 0) {//но дальше есть еще ветви
 			for (int i = 0; i < root->GetAmountofChildren(); i++) {
-				nodes = generatebranches1(root->GetChild(i), nodes, level + 1, neededlevel); //идти дальше по дереву
+				nodes = generatebranches1(root->GetChild(i), nodes, level + 1, neededlevel, totalnodes); //идти дальше по дереву
 			}
 		}
-		else {//дальше ветвей нет
-			if(totalnodes-nodes<10)
+		if ((!root->GetParent()) && (nodes == oldnodes)) {
+			if (totalnodes - nodes < 10) {
+				return nodes + totalnodes; //флаг для уничтожения дерева(см gentree)
+			}
+			else {
+				return -1; // дерево "успешно" сгенерировалось
+			}
 		}
 	}
-	return nodes - amount;
+	return nodes;
 }
-void gentree(int nodes) {
+
+Node* gentree(int nodes) {
 	Node* root = new Node(0);
 	int neededlevel = 0;
 	int totalnodes = nodes;
 	while (nodes > 0) {
 		nodes = generatebranches1(root, nodes, 0, neededlevel,totalnodes);
+		if (nodes >= totalnodes)goto bad_tree;
 		neededlevel++;
 	}
-	delete root;
+	bad_tree:
+	return root;
+}
+int find_leaves(Node* root) {
+	int leaves = 0;
+	if (root->GetAmountofChildren()) {
+		for (int i = 0; i < root->GetAmountofChildren(); i++) {
+			leaves += find_leaves(root->GetChild(i));
+		}
+		return leaves;
+	}
+	else {
+		return 1;
+	}
 }
 //void generatebranches(Node* root, int nodes) {
 //	int amount = 0;
@@ -102,6 +126,9 @@ void gentree(int nodes) {
 //	} while (nodes > 0);
 //}
 int main() {
-	gentree(5);
+	Node* root=nullptr;
+	int leaves=0;
+	root=gentree(20);
+	leaves=find_leaves(root);
 	return 0;
 }
